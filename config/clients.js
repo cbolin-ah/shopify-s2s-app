7a31c56af355f28add4c2c801f2109c6ede6c7b2
cbolin-ah <cbolin@audiohook.com>
2026-06-15 10:09:02 -0400

    Update clients.js

--- config/clients.js
+++ config/clients.js
@@ -1,6 +1,6 @@
 module.exports = {
   'client-test-server': {
-    audiohookId:   '9999999999',
+    audiohookId:   '99999999-9999-9999-9999-999999999999',
     webhookSecret: '41bcb984e85015bbe11ba8c832ba2f4ba49aae04b16f92f22cbbfdfc1725ae56',
   },
   'client-newbrand': {
